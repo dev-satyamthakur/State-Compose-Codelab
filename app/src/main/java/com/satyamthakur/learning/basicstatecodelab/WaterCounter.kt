868781c17f4ae97f7d278b8cd26926f3cff863fc
Satyam Thakur <sat.thakur20@gmail.com>
2023-10-13 17:06:00 +0530

    Making count var in WaterCounter a state for composable

--- app/src/main/java/com/satyamthakur/learning/basicstatecodelab/WaterCounter.kt
+++ app/src/main/java/com/satyamthakur/learning/basicstatecodelab/WaterCounter.kt
@@ -1,18 +1,29 @@
 package com.satyamthakur.learning.basicstatecodelab
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
 
 @Composable
-fun WaterCounter (modifier: Modifier = Modifier) {
-    val count = 0
-    Text(
-        text = "You have had $count glasses",
-        modifier = modifier.padding(16.dp)
-    )
+fun WaterCounter(modifier: Modifier = Modifier) {
+    Column(modifier = modifier.padding(16.dp)) {
+        var count by remember { mutableStateOf(0) }
+        Text(
+            text = "You have had $count glasses",
+            modifier = modifier.padding(16.dp)
+        )
+        Button(onClick = { count++ }, Modifier.padding(top = 8.dp)) {
+            Text("Add One")
+        }
+    }
 
 }
