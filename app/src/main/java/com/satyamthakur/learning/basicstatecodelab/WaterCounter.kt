79a9e8d0e8a5fb9f7adb93f17801fad4342fcf7d
Satyam Thakur <sat.thakur20@gmail.com>
2023-10-14 18:50:11 +0530

    Adding WellnessTaskItem composable and clear water count button inside WaterCounter

--- app/src/main/java/com/satyamthakur/learning/basicstatecodelab/WaterCounter.kt
+++ app/src/main/java/com/satyamthakur/learning/basicstatecodelab/WaterCounter.kt
@@ -1,6 +1,7 @@
 package com.satyamthakur.learning.basicstatecodelab
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
@@ -17,12 +18,29 @@ import androidx.compose.ui.unit.dp
 fun WaterCounter(modifier: Modifier = Modifier) {
     Column(modifier = modifier.padding(16.dp)) {
         var count by remember { mutableStateOf(0) }
-        Text(
-            text = "You have had $count glasses",
-            modifier = modifier.padding(16.dp)
-        )
-        Button(onClick = { count++ }, Modifier.padding(top = 8.dp)) {
-            Text("Add One")
+        if (count > 0) {
+            var showTask by remember { mutableStateOf(true) }
+            if (showTask) {
+                WellnessTaskItem(
+                    onClose = { showTask = false },
+                    taskName = "Have you taken your 15 min walk today?"
+                )
+            }
+            Text(
+                text = "You have had $count glasses",
+                modifier = modifier.padding(16.dp)
+            )
+        }
+        Row(Modifier.padding(top = 8.dp)) {
+            Button(onClick = { count++ }, enabled = count < 10) {
+                Text("Add one")
+            }
+            Button(
+                onClick = { count = 0 },
+                Modifier.padding(start = 8.dp)
+            ) {
+                Text("Clear water count")
+            }
         }
     }
 
